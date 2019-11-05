11b09eaeef2161ae190321c323f56cf0e4735f3c
GD3 <GD3@DESKTOP-KDJ6548>
2019-11-05 16:10:09 +0900

    팝업 레이어 구현

--- bluemarble/WebContent/popup/popup.js
+++ bluemarble/WebContent/popup/popup.js
@@ -1,10 +1,10 @@
-function buy_popup(el, title, value){
+function buy_popup(el, title, value){//증서구입
 
     var $el = $(el);        //레이어의 id를 $el 변수에 저장
     //var isDim = $el.prev().hasClass('dimBg');   //dimmed 레이어를 감지하기 위한 boolean 변수
 
     //isDim ? $('.dim-layer').fadeIn() : $el.fadeIn();
-    $('#dim-layer1').fadeIn();
+    $('#dim-layer-buy').fadeIn();
     var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
         $elHeight = ~~($el.outerHeight()),//팝업의 높이
         docWidth = $(document).width(),//브라우저 창 넓이
@@ -26,12 +26,12 @@ function buy_popup(el, title, value){
     $el.find('#buy').click(function(){
         //isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
     	//구매 함수 호출
-    	$('#dim-layer1').fadeOut();
+    	$('#dim-layer-buy').fadeOut();
         return true;
     });
     $el.find('#cancel').click(function(){
         //isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
-    	$('#dim-layer1').fadeOut();
+    	$('#dim-layer-buy').fadeOut();
         return false;
     });
    /*
@@ -41,13 +41,10 @@ function buy_popup(el, title, value){
     });
     */
 }
-function build_popup(el){
+function build_popup(el, title){//건물 구입
 
     var $el = $(el);        //레이어의 id를 $el 변수에 저장
-    //var isDim = $el.prev().hasClass('dimBg');   //dimmed 레이어를 감지하기 위한 boolean 변수
-
-    //isDim ? $('.dim-layer').fadeIn() : $el.fadeIn();
-    $('#dim-layer2').fadeIn();
+    $('#dim-layer-build').fadeIn();
     var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
         $elHeight = ~~($el.outerHeight()),//팝업의 높이
         docWidth = $(document).width(),//브라우저 창 넓이
@@ -63,30 +60,25 @@ function build_popup(el){
         $el.css({top: 0, left: 0});
     }
     
-    let $content = $('.pop-conts');
+    $el.find("#title").html(title);
     
     $el.find('#buy').click(function(){
-        //isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
     	//구매 함수 호출
-    	$('#dim-layer2').fadeOut();
+    	$('#dim-layer-build').fadeOut();
     	
         return true;
     });
     $el.find('#cancel').click(function(){
-        //isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
-    	$('#dim-layer2').fadeOut();
+    	$('#dim-layer-build').fadeOut();
         return false;
     });
 
 }
 
-function Loan_popup(el){
+function Loan_popup(el){//대출
 
     var $el = $(el);        //레이어의 id를 $el 변수에 저장
-    //var isDim = $el.prev().hasClass('dimBg');   //dimmed 레이어를 감지하기 위한 boolean 변수
-
-    //isDim ? $('.dim-layer').fadeIn() : $el.fadeIn();
-    $('#dim-layer2').fadeIn();
+    $('#dim-layer-loan').fadeIn();
     var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
         $elHeight = ~~($el.outerHeight()),//팝업의 높이
         docWidth = $(document).width(),//브라우저 창 넓이
@@ -102,24 +94,99 @@ function Loan_popup(el){
         $el.css({top: 0, left: 0});
     }
     
-    let $content = $('.pop-conts');
-    $content.empty();
-    $content.append("<input type='checkbox' value='hotel'>호텔");
-    $content.append("<input type='checkbox' value='building'>빌딩");
-    $content.append("<input type='checkbox' value='villa1'>별장");
-    $content.append("<input type='checkbox' value='villa2'>별장2채");
-    
     $el.find('#buy').click(function(){
-        //isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
-    	//구매 함수 호출
-    	$('#dim-layer2').fadeOut();
+        
+    	$('#dim-layer-loan').fadeOut();
     	
         return true;
     });
     $el.find('#cancel').click(function(){
-        //isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
-    	$('#dim-layer2').fadeOut();
+    	$('#dim-layer-loan').fadeOut();
         return false;
+    });
+
+}
+
+function Info_popup(el, title){//땅 클릭
+
+    var $el = $(el);        //레이어의 id를 $el 변수에 저장
+    $('#dim-layer-Info').fadeIn();
+    var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
+        $elHeight = ~~($el.outerHeight()),//팝업의 높이
+        docWidth = $(document).width(),//브라우저 창 넓이
+        docHeight = $(document).height(); //브라우저 창 높이
+
+    // 화면의 중앙에 레이어를 띄운다.
+    if ($elHeight < docHeight || $elWidth < docWidth) {
+        $el.css({
+            marginTop: -$elHeight /2,
+            marginLeft: -$elWidth/2
+        })
+    } else {//팝업창이 화면보다 클경우 마진을 지움
+        $el.css({top: 0, left: 0});
+    }
+
+    $el.find("#title").html(title);
+    $('.dim-layer').click(function(){
+        
+    	$('#dim-layer-Info').fadeOut();
+    	
+        return true;
+    });
+
+}
+function Gold_popup(el){//황금카드
+
+    var $el = $(el);        //레이어의 id를 $el 변수에 저장
+   
+    $('#dim-layer-gold').fadeIn();
+    var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
+        $elHeight = ~~($el.outerHeight()),//팝업의 높이
+        docWidth = $(document).width(),//브라우저 창 넓이
+        docHeight = $(document).height(); //브라우저 창 높이
+
+    // 화면의 중앙에 레이어를 띄운다.
+    if ($elHeight < docHeight || $elWidth < docWidth) {
+        $el.css({
+            marginTop: -$elHeight /2,
+            marginLeft: -$elWidth/2
+        })
+    } else {//팝업창이 화면보다 클경우 마진을 지움
+        $el.css({top: 0, left: 0});
+    }
+    $('.dim-layer').click(function(){
+    	
+    	$('#dim-layer-gold').fadeOut();
+    	
+        return true;
+    });
+
+}
+
+function Sale_popup(el){//매각
+
+    var $el = $(el);        //레이어의 id를 $el 변수에 저장
+   
+    $('#dim-layer-sale').fadeIn();
+    var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
+        $elHeight = ~~($el.outerHeight()),//팝업의 높이
+        docWidth = $(document).width(),//브라우저 창 넓이
+        docHeight = $(document).height(); //브라우저 창 높이
+
+    // 화면의 중앙에 레이어를 띄운다.
+    if ($elHeight < docHeight || $elWidth < docWidth) {
+        $el.css({
+            marginTop: -$elHeight /2,
+            marginLeft: -$elWidth/2
+        })
+    } else {//팝업창이 화면보다 클경우 마진을 지움
+        $el.css({top: 0, left: 0});
+    }
+    $el.find('#buy').click(function(){
+        
+    	$('#dim-layer-loan').fadeOut();
+    	
+        return true;
     });
 
 }
